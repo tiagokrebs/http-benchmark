12fe3b73c7307065a5cb60516c10b190a7eacb05
TK <tk@MacBook-Air-de-TK.local>
2021-09-16 07:57:57 -0300

    new version

--- locusfile.py
+++ locusfile.py
@@ -1,25 +1,6 @@
-from locust import HttpLocust, TaskSet, task, between
-import json
+from locust import HttpUser, task
 
-
-class UserBehavior(TaskSet):
-    # @task(1)
-    # def create_post(self):
-    #     headers = {'content-type': 'application/json', 'Accept-Encoding': 'gzip'}
-    #     self.client.post("/posts", data=json.dumps({
-    #         "title": "foo",
-    #         "body": "bar",
-    #         "userId": 1
-    #     }),
-    #                      headers=headers,
-    #                      name="Create a new post")
-
-    wait_time = between(3, 25)
-
-    @task(2)
-    def create_get(self):
-        self.client.get(url="/target.html", name="Create a new dummy get")
-
-
-class WebsiteUser(HttpLocust):
-    task_set = UserBehavior
+class HelloWorldUser(HttpUser):
+    @task
+    def hello_world(self):
+        self.client.get("/")
